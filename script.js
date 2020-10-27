9534a41bce89735e1d04cd6b775ea31ab25e4aba
Nehal Jain <nehalj98@gmail.com>
2020-10-27 14:19:56 -0700

    m

--- script.js
+++ script.js
@@ -105,24 +105,24 @@
             });
         });
 
-        // var coverImage = document.querySelector("#cover-image");
+        var coverImage = document.querySelector("#cover-image");
     
-        // function setTranslate(xPos, yPos, el) {
-        //     el.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
-        // }
+        function setTranslate(xPos, yPos, el) {
+            el.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
+        }
     
-        // window.addEventListener("DOMContentLoaded", scrollLoop, false);
+        window.addEventListener("DOMContentLoaded", scrollLoop, false);
     
-        // var xScrollPosition;
-        // var yScrollPosition;
+        var xScrollPosition;
+        var yScrollPosition;
     
-        // function scrollLoop() {
-        //     xScrollPosition = window.scrollX;
-        //     yScrollPosition = window.scrollY;
+        function scrollLoop() {
+            xScrollPosition = window.scrollX;
+            yScrollPosition = window.scrollY;
     
-        //     setTranslate(0, yScrollPosition * -0.5, coverImage);
+            setTranslate(0, yScrollPosition * -0.5, coverImage);
     
-        //     requestAnimationFrame(scrollLoop);
-        // }
+            requestAnimationFrame(scrollLoop);
+        }
 
     })();
